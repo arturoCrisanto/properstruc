54ebde583d1f9cf5f57ad78b456abe50feaf1556
ruzell <arturocrisanto1@gmail.com>
2025-06-27 01:57:18 +0800

    feat: configure image domains for Google profile pictures

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "lh3.googleusercontent.com", // Google
+      "platform-lookaside.fbsbx.com", // Facebook profile images
+      "scontent.xx.fbcdn.net", // Sometimes used by Facebook too
+    ],
+  },
+};
 
 export default nextConfig;
